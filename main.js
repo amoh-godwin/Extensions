43d5ce334091bc6d4d39a4319d2377642e8d02c8
amoh-godwin <amohgyebigodwin@gmail.com>
2018-07-28 17:24:07 +0000

    ...

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 var img_ele = document.getElementsByClassName('FFVAD').item(0);
-alert(img_ele.src);
+
+function onStartedDownload(id) {
+    alert('started');
+    console.log(`Started downloading: ${id}`);
+}
+
+function onFailed(error) {
+    console.log(`Download failed: ${error}`);
+}
+
+var downloadUrl = img_ele.src;
+
+var downloading = browser.downloads.download({
+    url : downloadUrl,
+    filename : 'my-image-again.jpg',
+    conflictAction : 'uniquify'
+});
+
+downloading.then(onStartedDownload, onFailed);
